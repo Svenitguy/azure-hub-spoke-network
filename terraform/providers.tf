42b419aa7c33f25204e5546ea8a249ffdc659636
Svenitguy <sven_velleman@outlook.be>
2026-09-22 00:28:27 +0200

    fix: dubbele compute resources verwijderd uit providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,155 +1,28 @@
-# =====================================================================
-# 0. AUTOMATISCHE CLOUD-NATIVE SSH SLEUTEL GENERATIE (DevSecOps)
-# =====================================================================
-resource "tls_private_key" "vm_ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0" # Moderne 2026 azurerm provider v4x
+    }
+    # DEVSECOPS INTEGRATIE: Voeg de TLS provider toe voor automatische sleutelgeneratie
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
+  }
 
-# =====================================================================
-# 1. AZURE BASTION INFRASTRUCTUUR
-# =====================================================================
-resource "azurerm_public_ip" "bastion_pip" {
-  name                = "pip-bst-hub-prod-tf"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = var.tags
-}
-
-resource "azurerm_bastion_host" "bastion" {
-  name                = "bst-hub-prod-tf"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Basic"
-  tags                = var.tags
-
-  ip_configuration {
-    name                 = "IpConf"
-    subnet_id            = azurerm_subnet.hub_bastion.id
-    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+  # Dit is de cloud-backend voor je Terraform-geheugen
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatesveniac"
+    container_name       = "tfstate-hub-spoke"
+    key                  = "hub-spoke.terraform.tfstate"
   }
 }
 
-# =====================================================================
-# 2. VIRTUELE MACHINE: SPOKE 1 (PROD)
-# =====================================================================
-resource "azurerm_network_interface" "vm_spoke1_nic" {
-  name                = "vm-spoke1-prod-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.spoke1_apps.id
-    private_ip_address_allocation = "Dynamic"
-  }
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_linux_virtual_machine" "vm_spoke1" {
-  name                = "vm-spoke1-prod"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1ls"
-  admin_username      = "azureuser"
-
-  # DEVSECOPS FIX: Wachtwoordauthenticatie uitgeschakeld
-  disable_password_authentication = true
-
-  # ENTERPRISE AUTOMATISERING: Verwijst nu direct naar de TLS provider resource
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.vm_ssh_key.public_key_openssh
-  }
-
-  network_interface_ids = [azurerm_network_interface.vm_spoke1_nic.id]
-  tags                  = var.tags
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS" # Goedkope HDD voor de lab-omgeving
-  }
-
-  source_image_reference {
-    publisher = "canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
-    version   = "latest"
-  }
-}
-
-resource "azurerm_dev_test_global_vm_shutdown_schedule" "shutdown_spoke1" {
-  virtual_machine_id = azurerm_linux_virtual_machine.vm_spoke1.id
-  location           = azurerm_resource_group.rg.location
-  enabled            = true
-
-  daily_recurrence_time = "1800" # 18:00 uur uitschakelen (FinOps)
-  timezone              = "Romance Standard Time"
-
-  notification_settings {
-    enabled = false
-  }
-}
-
-# =====================================================================
-# 3. VIRTUELE MACHINE: SPOKE 2 (TEST)
-# =====================================================================
-resource "azurerm_network_interface" "vm_spoke2_nic" {
-  name                = "vm-spoke2-test-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.spoke2_apps.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm_spoke2" {
-  name                = "vm-spoke2-test"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  size                = "Standard_B1ls"
-  admin_username      = "azureuser"
-
-  # DEVSECOPS FIX: Wachtwoordauthenticatie uitgeschakeld
-  disable_password_authentication = true
-
-  # ENTERPRISE AUTOMATISERING: Verwijst nu direct naar de TLS provider resource
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.vm_ssh_key.public_key_openssh
-  }
-
-  network_interface_ids = [azurerm_network_interface.vm_spoke2_nic.id]
-  tags                  = var.tags
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
-    version   = "latest"
-  }
-}
-
-resource "azurerm_dev_test_global_vm_shutdown_schedule" "shutdown_spoke2" {
-  virtual_machine_id = azurerm_linux_virtual_machine.vm_spoke2.id
-  location           = azurerm_resource_group.rg.location
-  enabled            = true
-
-  daily_recurrence_time = "1800"
-  timezone              = "Romance Standard Time"
-
-  notification_settings {
-    enabled = false
-  }
-}
+# De TLS provider initialiseren (heeft geen extra instellingen nodig)
+provider "tls" {}
